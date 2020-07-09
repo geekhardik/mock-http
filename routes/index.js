79712584727ca1a7352cb23689b0f6edd4a105f3
hardiks <hardiks@splunk.com>
2020-07-09 14:55:07 -0700

    one more path

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,9 @@ router.post('/v2/event', function(req, res, next) {
   res.status(200).send('"OK"')
 });
 
+/* GET home page. */
+router.post('v2/datapoint', function(req, res, next) {
+  res.status(200).send('"OK"')
+});
+
 module.exports = router;
